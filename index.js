cd17cc0f07c257d5effd7074bbc32bfbd892260a
Hezron Nyamboga <hezzyn1@gmail.com>
2024-01-14 15:54:44 +0300

    connect to mongoDB

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// database
+const connectDB = require('./config/connectDB')
 
 
 const port = process.env.PORT || 5000;
 
 const startApp = async () => {
 try{
+	await connectDB(process.env.MONGO_URL)
 	app.listen(port, () => console.log(`Server is listen on port ${port}`)
 )
 } catch(error){
