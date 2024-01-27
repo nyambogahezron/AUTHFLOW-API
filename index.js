eabeb7c04380af40fee0c0a895b28e254426b74f
Hezron Nyamboga <hezzyn1@gmail.com>
2024-01-27 18:01:58 +0300

    server frontend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('express-async-errors');
-
+const path = require('path')
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
@@ -20,17 +20,33 @@ const notFoundMiddleware = require('./middleware/notFound');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
 const asyncHandlerMiddleware = require('./middleware/asyncHandler');
 
-app.use(cors({
-  origin: ['https://auth-flow-app-0dsk.onrender.com', 'https://authflow-app-rho.vercel.app/login'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: ['https://auth-flow-app-0dsk.onrender.com', 'https://authflow-app-rho.vercel.app/login'],
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// }));
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(cookieParser(process.env.JWT_SECRET));
+
+//Serve frontend files
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRouter);
